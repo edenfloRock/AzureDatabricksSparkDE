d3a86969cdb74c1e8812fbe9bc17ae5557f82d20
edenflo.proyectos@gmail.com <edenflo.proyectos@gmail.com>
2024-12-18 00:00:39 +0000

    Add ingest_driver_file file
    Add ingest_driver_file file

---
+++ ingestion/4 Ingest_drivers_file.py
@@ -0,0 +1,94 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Ingest drivers.json file 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Read the JSON file using the spark dataframe reader API
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+
+# COMMAND ----------
+
+name_schema = StructType(fields=[    
+    StructField("forename", StringType(), True),
+    StructField("surname", StringType(), True)
+])
+
+# COMMAND ----------
+
+drivers_schema = StructType(fields=[    
+    StructField("driverId", IntegerType(), True),
+    StructField("driverRef", StringType(), True),
+    StructField("number", IntegerType(), True),
+    StructField("code", StringType(), True),
+    StructField("name", name_schema),
+    StructField("dob", DateType(), True),
+    StructField("nationality", StringType(), True),
+    StructField("url", StringType(), True),
+])
+
+# COMMAND ----------
+
+drivers_df = spark.read.json("dbfs:/mnt/edenflostoragedata/raw/drivers.json", schema=drivers_schema)
+display(drivers_df)
+
+# COMMAND ----------
+
+drivers_df.printSchema()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 -  Renamed columns and add new columns
+# MAGIC 1. driverid renamed to driver_id
+# MAGIC 2. driverRef renamed to driver_ref
+# MAGIC 3. ingestion_date added
+# MAGIC 4. name added with concatenation of forname and surname
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, concat, current_timestamp, lit
+
+# COMMAND ----------
+
+drivers_with_columns_df = drivers_df \
+    .withColumnRenamed("driverId", "driver_id") \
+    .withColumnRenamed("driverRef", "driver_ref") \
+    .withColumn("ingestion_date", current_timestamp()) \
+    .withColumn("name", concat(col("name.forename"), lit(" "), col("name.surname")))
+
+display(drivers_with_columns_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3- Drop the unwanted columns
+# MAGIC 1. name.forname
+# MAGIC 2. name.surname
+# MAGIC 3. url
+
+# COMMAND ----------
+
+drivers_final_df = drivers_with_columns_df.drop("namer.forename", "namer.surname", "url")
+display(drivers_final_df)
+
+# COMMAND ----------
+
+drivers_final_df.write.mode("overwrite").parquet("/mnt/edenflostoragedata/processed/drivers")
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/edenflostoragedata/processed/drivers
+
+# COMMAND ----------
+
+display(spark.read.parquet("/mnt/edenflostoragedata/processed/drivers"))
+
+# COMMAND ----------
+
+
